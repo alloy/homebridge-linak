e89ed2b429fde4a2043d3db5f7dfc456dba57e47
Eloy Durán <eloy.de.enige@gmail.com>
2021-09-03 00:38:48 +0200

    First attempt at getting value from the desk

--- src/platform.ts
+++ src/platform.ts
@@ -1,118 +1,129 @@
-import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic, PlatformAccessoryEvent } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
 
-import {} from "deskbluez"
+import * as deskbluez from 'deskbluez';
 
-interface ConnectedDevice {
-  name: string
-  address: string
-  modelName: string
+interface DeskConfig {
+  name: string;
+  address: string;
+  modelName: string;
 }
+
+interface LinakDeskControlPlatformConfig extends PlatformConfig {
+  desks: DeskConfig[];
+}
+
+export type LinakDeskPlatformAccessory = PlatformAccessory<DeskConfig>;
+
+
 export class LinakDeskControlPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
   // this is used to track restored cached accessories
-  public readonly accessories: PlatformAccessory[] = [];
+  public readonly accessories: LinakDeskPlatformAccessory[] = [];
+
+  public readonly bluetooth: deskbluez.Bluetooth;
 
   constructor(
     public readonly log: Logger,
-    public readonly config: PlatformConfig,
+    public readonly config: LinakDeskControlPlatformConfig,
     public readonly api: API,
   ) {
+    this.bluetooth = new deskbluez.Bluetooth();
     this.log.debug('Finished initializing platform:', this.config.name);
-
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
-    // this.api.on('didFinishLaunching', () => {
-    //   log.debug('Executed didFinishLaunching callback');
-    //   // run the method to discover / register your devices as accessories
-    //   this.discoverDevices();
-    // });
+    this.log.debug('Desks:', JSON.stringify(this.config.desks));
+    this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
   }
 
   /**
    * This function is invoked when homebridge restores cached accessories from disk at startup.
    * It should be used to setup event handlers for characteristics and update respective values.
    */
-  configureAccessory(accessory: PlatformAccessory) {
+  configureAccessory(accessory: LinakDeskPlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
 
     // add the restored accessory to the accessories cache so we can track if it has already been registered
     this.accessories.push(accessory);
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
+  // When this event is fired it means Homebridge has restored all cached accessories from disk.
+  // Dynamic Platform plugins should only register new accessories after this event was fired,
+  // in order to ensure they weren't added to homebridge already. This event can also be used
+  // to start discovery of new accessories.
+  didFinishLaunching() {
+    this.log.debug('Executed didFinishLaunching callback');
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
-
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
-
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+    this.config.desks.forEach(desk => {
+      const uuid = this.api.hap.uuid.generate(desk.address);
+      this.log.debug('Linak Desk:', JSON.stringify({ ...desk, uuid }));
+      let accessory = this.accessories.find(accessory => accessory.UUID === uuid);
+      if (accessory) {
+        this.log.info('Restoring existing desk accessory from cache:', accessory.displayName);
+        // TODO: this.setupAccessory(accessory); Is this necessary or what is cached?
       } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
+        this.log.info('Adding new desk accessory:', desk.name);
+        accessory = new this.api.platformAccessory<DeskConfig>(desk.name, uuid);
+        accessory.context = desk;
+        this.setupAccessory(accessory);
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
+    });
+  }
+
+  setupAccessory(accessory: LinakDeskPlatformAccessory) {
+    accessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
+      this.log.info('Identify requested.', accessory.displayName);
+    });
+
+    const hap = this.api.hap;
+
+    const accInfo = accessory.getService(hap.Service.AccessoryInformation);
+    if (accInfo) {
+      accInfo.setCharacteristic(hap.Characteristic.Manufacturer, 'Linak');
+      accInfo.setCharacteristic(hap.Characteristic.Model, accessory.context.modelName);
     }
+
+    /**
+     * Working with blinds, taken from https://github.com/dxdc/homebridge-blinds/blob/master/index.js
+     */
+    const service = new hap.Service.WindowCovering(accessory.context.name);
+    service.getCharacteristic(hap.Characteristic.CurrentPosition).on('get', async (callback) => {
+      const desk = await this.connectDesk(accessory.context);
+      const state = await desk.state();
+      await desk.disconnect();
+      callback(null, state.cm);
+    });
+    service
+      .getCharacteristic(hap.Characteristic.TargetPosition)
+      .on('get', (callback) => {
+        this.log.info('Get desk target height');
+        callback(null, undefined);
+      })
+      .on('set', (value) => {
+        this.log.info('Set desk target height:', value.valueOf());
+      });
+    service.getCharacteristic(hap.Characteristic.PositionState).updateValue(hap.Characteristic.PositionState.STOPPED);
+    service.getCharacteristic(hap.Characteristic.ObstructionDetected).updateValue(false);
+  }
+
+  /**
+   * Code taken largely verbatim from https://github.com/alex20465/deskbluez/blob/master/src/lib/cli.ts
+   * TODO: Refactor deskbluez to make these available as a lib instead of defined in CLI
+   */
+
+  private async connectDesk(deskConfig: DeskConfig) {
+    const model = deskbluez.factory.getDeskModel(deskConfig.modelName);
+    this.log.debug('Connecting to desk:', JSON.stringify({ deskConfig, model }));
+    await this.bluetooth.startDiscovery();
+    const bluetoothDevice = await this.bluetooth.connect(deskConfig.address);
+    await this.bluetooth.stopDiscovery();
+    const desk = deskbluez.factory.createDesk(model, bluetoothDevice);
+
+    await desk.connect();
+    await desk.subscribe();
+
+    return desk;
   }
 }
